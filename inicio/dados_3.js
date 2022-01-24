cf90c34bc200d5bf0bfb4bb50861411040b9b3bc
Emily Marcelino Inácio <55949281+emiinacio@users.noreply.github.com>
2022-01-24 18:50:50 +0000

    new exercice of javascript

---
+++ inicio/dados_3.js
@@ -0,0 +1,16 @@
+let idade = 32;
+console.log(typeof 31);
+console.log(typeof idade);
+
+//numeros inteiros e números com casas décimais, são iguais!
+
+let salario = 4578.32;
+console.log(typeof salario);
+
+//Bollean
+let estaChovendo = true; //ou false
+console.log(typeof estaChovendo);
+
+//String
+console.log(typeof "Teste");
+console.log(typeof 'Teste');
---
+++ inicio/desafio_area.js
@@ -0,0 +1,10 @@
+// PI * raio * rario
+
+const PI = 3.141592;
+let raio = 20;
+let areaCirc = PI * raio * raio;
+
+console.log("O resultado da área é: " + areaCirc + " m2.");
+
+areaCirc = Math.PI * raio * raio;
+console.log("O resultado da área é: " + areaCirc + " m2.");
