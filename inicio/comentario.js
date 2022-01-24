7285cb08d00f1d57d21c007aceb8c0f2ba721bc1
Emily Marcelino Inácio <55949281+emiinacio@users.noreply.github.com>
2022-01-24 12:32:57 +0000

    update initial

---
+++ inicio/comentario.js
@@ -0,0 +1,9 @@
+//consolog.log é usado para exibit algo na tela do computador!
+console.log("Passo #3");
+console.log("Passo #1"); //mais um exemplo de console.log
+//console.log("Passo #2");
+
+/*
+console.log("Passo #3");
+console.log("Passo #1");
+*/
