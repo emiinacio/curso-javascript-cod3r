235aa3be207312941ddeb5b8ea1a52702c4d6f30
Emily Marcelino Inácio <55949281+emiinacio@users.noreply.github.com>
2022-01-27 12:11:42 +0000

    finally fundaments javascript

---
+++ fundamentos/erro.js
@@ -0,0 +1,23 @@
+function tratarErroELancar(erro) {
+    //throw new Error('...')
+    //throw 10
+    //throw true
+    //throw 'mensagem'
+    throw {
+        nome: erro.name,
+        msg: erro.message,
+    }
+}
+
+function imprimirNomeGritado(obj) {
+    try {
+        console.log(obj.nome.toUpperCase() + '!!!')
+    } catch(e){
+        tratarErroELancar(e)
+    } finally {
+        console.log('final')
+    }
+}
+
+const obj = { nome: 'Roberto'}
+imprimirNomeGritado(obj)
---
+++ fundamentos/operadoresRelacionais.js
@@ -0,0 +1,17 @@
+console.log('01) ', '1' == 1) //iguais
+console.log('02) ', '1' === 1) //diferentes
+console.log('03) ', '3' != 3) //iguais
+console.log('04) ', '3' !== 3) //diferentes
+
+console.log('05) ', '3' < 2) //falso
+console.log('06) ', '3' > 2) //verdadeiro
+console.log('07) ', '3' <= 2) //falso
+console.log('08) ', '3' >= 2) //verdadeiro
+
+const d1 = new Date(0)
+const d2 = new Date(0)
+console.log('09) ', d1 === d2) //falso
+console.log('10) ', d1 == d2) //falso
+console.log('11) ', d1.getTime() === d2.getTime()) //verdadeiro
+console.log('12) ', undefined == null) //verdadeiro
+console.log('11) ', undefined === null) //falso
