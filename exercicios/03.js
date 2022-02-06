4127da6eaa30db5412d09af17cae320349da598e
Emily Marcelino Inácio <55949281+emiinacio@users.noreply.github.com>
2022-02-06 20:50:16 +0000

    new update exercise

--- exercicios/03.js
+++ exercicios/03.js
@@ -2,3 +2,12 @@
 Crie uma função que recebe dois parâmetros, 
 base e expoente, e retorne a base elevada ao expoente.
 */
+function potenciacao(base, expoente) {
+    let resultado = Math.pow(base, expoente)
+    resultado = base ** expoente
+
+    return resultado
+}
+
+console.log(potenciacao(2, 2))
+console.log(potenciacao(4, 2))
