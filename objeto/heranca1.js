7e486578bd97a4f983d4a6f5a31717d578df0ec6
Emily Marcelino Inácio <55949281+emiinacio@users.noreply.github.com>
2022-02-09 18:46:46 +0000

    OO module completion

--- objeto/heranca1.js
+++ objeto/heranca1.js
@@ -8,4 +8,12 @@ const volvo = {
     velMax: 200
 }
 
+console.log(ferrari.prototype)
 console.log(ferrari.__proto__)
+console.log(ferrari.__proto__ === Object.prototype)
+console.log(volvo.__proto__ ===Object.prototype)
+console.log(Object.prototype.__proto__)
+
+function MeuObjeto() {}
+console.log(typeof Object, typeof MeuObjeto)
+console.log(Object.prototype, MeuObjeto.prototype)
