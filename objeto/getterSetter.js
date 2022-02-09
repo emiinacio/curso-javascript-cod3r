3564ec19b4fa8cf68d6748aa2ca9d91218118e44
Emily Marcelino Inácio <55949281+emiinacio@users.noreply.github.com>
2022-02-09 15:50:40 +0000

    new module OO

---
+++ objeto/getterSetter.js
@@ -0,0 +1,25 @@
+/*
+GET
+para ler o valor de uma variavel
+*/
+
+/*
+SET
+para alterar o valor de uma variavel
+*/
+
+const sequencia = {
+    _valor: 1, // convenção
+    get valor() { return this._valor++ },
+    set valor(valor) { 
+        if(valor > this._valor) {
+            this._valor = valor 
+        }
+    }
+}
+
+console.log(sequencia.valor, sequencia.valor)
+sequencia.valor = 1000
+console.log(sequencia.valor, sequencia.valor)
+sequencia.valor = 900
+console.log(sequencia.valor, sequencia.valor)
---
+++ objeto/oo.js
@@ -0,0 +1,22 @@
+//  CÓDIGO NÃO EXECUTÁVEL!!
+
+// Procedural
+processamento(valor1, valor2, valor3)
+
+// OO
+objeto = {
+    valor1,
+    valor2,
+    valor3,
+    procesamento() {
+
+    }
+}
+
+objeto.processamento() //Foco passou a ser o objeto
+
+// Principios importes:
+// 1. Abstração
+// 2. Encapsulamento
+// 3. Herançã (prototype)
+// 4. Polimorfismo
