ea089c47fdc34e42c50704ccd0133b9f140e098f
Emily Marcelino Inácio <55949281+emiinacio@users.noreply.github.com>
2022-02-17 15:56:47 +0000

    es6 part 2

---
+++ esnext/set.js
@@ -0,0 +1,17 @@
+// Estrutura em conjunto
+//não aceita repetição / não indexada
+const times = new Set()
+times.add('Vasco')
+times.add('São Paulo').add('Palmeiras').add('Cortinthians')
+times.add('Flamengo')
+times.add('Vasco')
+
+console.log(times)
+console.log(times.size)
+console.log(times.has('Flamengo'))
+times.delete('Flamengo')
+console.log(times.has('Flamengo'))
+
+const nomes = ['Raquel', 'Lucas', 'Julia', 'Lucas']
+const nomesSet = new Set(nomes)
+console.log(nomesSet)
