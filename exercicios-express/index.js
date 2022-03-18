aa17ca35e88f8a2357ce7a3914d67d056e9958a9
emiinacio <emilymarcelino59@gmail.com>
2022-03-18 18:17:43 +0000

    types of answer

--- exercicios-express/index.js
+++ exercicios-express/index.js
@@ -1,6 +1,35 @@
 const express = require('express');
 const app = express();
 
+app.get('/opa', (req, res) => {
+
+    res.json({
+        data: [
+            {id: 7, name: 'Ana', position:1},
+            {id: 34, name: 'Bia', position: 2},
+            {id: 73, name: 'Carlos', position: 3}
+        ],
+        count: 30,
+        skip: 0,
+        limit: 3,
+        status: 200
+    })
+
+    // res.json([
+    //     {id: 7, name: 'Ana', position:1},
+    //     {id: 34, name: 'Bia', position: 2},
+    //     {id: 73, name: 'Carlos', position: 3}
+    // ])
+
+    // res.json({
+    //     name: 'ipad 32Gb',
+    //     preco: 1899,
+    //     discount: 0.12
+    // })
+
+    // res.send('<h1>Estou bem</h1><br></br><p>Teste</p>')
+})
+
 app.listen(8080, () => {
     console.log('Backend executando...')
 })
